7e90d9d5f0ea85301ff3a633850f3ffdb96cbbf9
luiz <jose.iscosistemas@gmail.com>
2023-10-02 10:39:21 -0700

    First commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from './app'
+import { env } from '@/env'
+app
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then((address) => {
+    console.log(`🚀  Server listening on: ${address}`)
+  })
